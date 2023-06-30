f5a3a31b14e922d04c3ef4b7bea40d0f1dd78916
Wenisch Herbert <herbert.wenisch@t-online.de>
2023-06-30 13:45:14 +0200

    Initial sharing of project

---
+++ Account.java
@@ -0,0 +1,42 @@
+/**
+ * @author (Herbert Wenisch)
+ * @version (30-06-23)
+ */
+
+import java.text.DecimalFormat;
+
+public class Account{
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    private int no;
+    private String owner;
+    private double balance;
+    private double turnover;
+    private final double turnoverMax = 1500;
+    
+    public Account(int no, String owner){
+        this.no = no;
+        this.owner = owner;
+        this.balance = 2000;
+    }
+    
+    public boolean payIn(double amount){
+        return true;
+    }
+    
+    public boolean payOut(double amount){
+        return true;
+    }
+    
+    public boolean transfer(Account goal, double amount){
+        return true;
+    }
+    
+    @Override
+    public String toString(){
+        String out = "Inhaber: " +  owner + "\n";
+        out += "Kontonummer: " + no + "\n";
+        out += "aktueller Kontostand: " + df.format(balance) + " €" + "\n";
+        out += "bisheriger Tagesumsatz: " + df.format(turnover)  + " €";
+        return out;
+    }
+}
