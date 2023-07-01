97e832176ff6e03e87d25f811e881374c7989f43
HerbertWenisch <herbert.wenisch@t-online.de>
2023-07-01 12:34:18 +0200

    "Bug in transfer fixed!"

--- Account.java
+++ Account.java
@@ -38,6 +38,7 @@ public class Account{
         if(!goal.payIn(amount)){
             turnover -= amount; // roll back
             payIn(amount);
+            turnover -= amount;
             return false;     
         }
         return true;
